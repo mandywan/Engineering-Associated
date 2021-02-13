a6b42c4d4c074ae1149c059b4634f75a1331b5ed
George Zhang <43013270+geo278@users.noreply.github.com>
2021-02-13 15:28:41 -0800

    test endpoint debugger, move employee to domain folder

--- AeDirectory/Controllers/DefaultController.cs
+++ AeDirectory/Controllers/DefaultController.cs
@@ -15,12 +15,25 @@ namespace QuickStart.Controllers
 			_logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
+		[Route("test1")]
 		[HttpGet]
-		public object Get()
+		public object Get1()
 		{
 			var responseObject = new
 			{
-				Status = "Up"
+				Status = "Up 1"
+			};
+			_logger.LogInformation($"Status pinged: {responseObject.Status}");
+			return responseObject;
+		}
+
+		[Route("test2")]
+		[HttpGet]
+		public object Get2()
+		{
+			var responseObject = new
+			{
+				Status = "Up22"
 			};
 			_logger.LogInformation($"Status pinged: {responseObject.Status}");
 			return responseObject;
