481c06015e54d5a53338772dd6399c2c10ac9043
Yang Xia <yxia@bccrc.ca>
2021-03-19 19:42:45 -0700

    added additional return fields for org chart

--- backend/Domain/OrgChartEmployee.cs
+++ backend/Domain/OrgChartEmployee.cs
@@ -9,7 +9,10 @@ namespace AeDirectory.Domain
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string PhotoUrl { get; set; }
+        public string Title { get; set; }
         public string CompanyCode { get; set; }
+         public string OfficeCode { get; set; }
+        public string GroupCode { get; set; }
         public bool IsContractor { get; set; }        
         public int Level { get; set; }
 
