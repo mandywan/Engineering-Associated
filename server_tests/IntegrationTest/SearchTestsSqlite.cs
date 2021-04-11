9599b9ddffb0bed7d7049fe85662b5c06633ea30
Yang Xia <xiazcy@gmail.com>
2021-04-10 18:16:54 -0700

    .

--- server_tests/IntegrationTest/SearchTestsSqlite.cs
+++ server_tests/IntegrationTest/SearchTestsSqlite.cs
@@ -23,7 +23,7 @@ namespace IntegrationTest
         }
         
         [Fact]
-        public async Task SearchCompany_ShouldReturnCorrectData()
+        public async Task SearchByCompany_ShouldReturnCorrectData()
         {
             // Arrange
             var client = _factory.CreateClient();
@@ -48,7 +48,72 @@ namespace IntegrationTest
             Assert.Equal("", searchRes.GetValue("msg"));
             Assert.Contains(employees, e => e.LastName == "Schaden");
             Assert.Contains(employees, e => e.FirstName == "Arletha");
+            foreach (EmployeeDTO e in employees) {
+                Assert.Equal("01", e.CompanyCode);
+            }
+        }
 
+        [Fact]
+        public async Task SearchByOffice_ShouldReturnCorrectData()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var payload = "{\"Office\":{\"type\":\"OR\",\"values\":[{\"CompanyId\":\"02\", \"OfficeId\": \"02\"}]}}";
+            HttpContent post = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("/api/search", post);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine(stringResponse);
+            var searchRes = JsonConvert.DeserializeObject<JObject>(stringResponse);
+            // Console.WriteLine(searchRes.GetValue("results"));
+            var employees = JsonConvert.DeserializeObject<ICollection<EmployeeDTO>>(searchRes.GetValue("results").ToString());
+            
+            Assert.Equal(11, searchRes.GetValue("total"));
+            Assert.Equal("", searchRes.GetValue("msg"));
+            Assert.Contains(employees, e => e.LastName == "Price");
+            Assert.Contains(employees, e => e.FirstName == "Hassan");
+            foreach (EmployeeDTO e in employees) {
+                Assert.Equal("02", e.CompanyCode);
+                Assert.Equal("02", e.OfficeCode);
+            }
+        }
+
+        [Fact]
+        public async Task SearchByGroup_ShouldReturnCorrectData()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var payload = "{\"Group\":{\"type\":\"OR\",\"values\":[{\"CompanyId\":\"03\", \"OfficeId\": \"05\", \"GroupId\": \"09\"}]}}";
+            HttpContent post = new StringContent(payload, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("/api/search", post);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            // Console.WriteLine(stringResponse);
+            var searchRes = JsonConvert.DeserializeObject<JObject>(stringResponse);
+            // Console.WriteLine(searchRes.GetValue("results"));
+            var employees = JsonConvert.DeserializeObject<ICollection<EmployeeDTO>>(searchRes.GetValue("results").ToString());
+            
+            Assert.Equal(4, searchRes.GetValue("total"));
+            Assert.Equal("", searchRes.GetValue("msg"));
+            Assert.Contains(employees, e => e.LastName == "Steuber");
+            Assert.Contains(employees, e => e.FirstName == "Cristy");
+            foreach (EmployeeDTO e in employees) {
+                Assert.Equal("03", e.CompanyCode);
+                Assert.Equal("05", e.OfficeCode);
+                Assert.Equal("09", e.GroupCode);
+            }
         }
 
         [Fact]
