4794b4c88062e2b84c696c7e107b706914892147
Xin Yi Wang <helenwangcat@hotmail.com>
2021-04-12 19:34:44 -0700

    changed photo upload button handling

--- frontend/src/ui/components/image-upload/image-upload.js
+++ frontend/src/ui/components/image-upload/image-upload.js
@@ -2,10 +2,13 @@ import React, {useState} from 'react'
 import {Button, Box} from "@material-ui/core";
 import {uploadImage} from "../../../services/image-upload";
 import axios from "axios";
+import CheckCircleOutlineSharpIcon from '@material-ui/icons/CheckCircleOutlineSharp';
 
 const ImageUpload = (props) => {
     const [selectedFile, setSelectedFile] = useState(null);
     const fileInput = React.useRef(null);
+    const [isUploading, setIsUploading] = useState(false)
+    const [isUploaded, setIsUploaded] = useState(false)
 
     const fileChangedHandler = (event) => {
         // cannot be larger than 2M
@@ -13,6 +16,7 @@ const ImageUpload = (props) => {
             alert("File is too big! Maximum file size is 2MB.");
         } else {
             setSelectedFile(event.target.files[0]);
+            setIsUploaded(false);
         }
     }
 
@@ -23,6 +27,7 @@ const ImageUpload = (props) => {
     // TODO: ux improvement during file upload
     const uploadHandler = (event) => {
         // create unique fileName with time stamp. fileName is used as key for s3 db
+        setIsUploading(true)
         const keys = selectedFile.name.split('.');
         const fileName = new Date().getTime().toString() + "." + keys[keys.length-1];
 
@@ -33,9 +38,11 @@ const ImageUpload = (props) => {
             if (res.status === 200) {
                 // update imageName in parent component
                 props.passImageName(res.data)
+                setIsUploaded(true);
             } else {
                 alert("File upload unsuccessful. Please try again.")
             }
+            setIsUploading(false)
         })
     }
 
@@ -61,6 +68,7 @@ const ImageUpload = (props) => {
             </Box>
             <Box>
                 <Button
+                    disabled={!selectedFile || isUploading}
                     variant={"contained"}
                     size={"medium"}
                     color={"primary"}
@@ -68,6 +76,11 @@ const ImageUpload = (props) => {
                     text={"Upload"}>
                     Upload
                 </Button>
+                <CheckCircleOutlineSharpIcon
+                    style={{ display: isUploaded ? 'inline' : 'none' }}
+                    color="secondary"
+                    >
+                </CheckCircleOutlineSharpIcon>
             </Box>
         </div>
     );
